300f65816cc1d5650981acfceb043ce0cf764e27
Rajesh04-26 <singhrajeshku6@gmail.com>
2025-09-04 08:11:34 +0530

    Final Project Commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
           <a href="https://github.com/Rajesh04-26" target="_blank" rel="noopener noreferrer">
             <GitHubIcon />
           </a>
-          <a href="www.linkedin.com/in/rajesh-kumar-singh-1549112b9" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/rajesh-kumar-singh-1549112b9/overlay/about-this-profile/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BNr626%2BmVSpaByZNYcrevqg%3D%3D" target="_blank" rel="noopener noreferrer">
             <LinkedInIcon />
           </a>
         </div>
